a348cc338e95f58ac2c5399c14be7c9db5ad5134
satheesh12sk <satheesh@navadhiti.com>
2024-12-10 12:57:53 +0530

    Commiiting with home page and test class

--- src/test/java/testPackage/TC_002_HomePage.java
+++ src/test/java/testPackage/TC_002_HomePage.java
@@ -17,7 +17,7 @@ public class TC_002_HomePage extends BaseClass {
 	}
 
 	@Test(dataProvider = "sendData")
-	public void validHomepage(String testNameDetails, String authorName, String category, String mobilenumber) {
+	public void validHomepage(String testNameDetails, String authorName, String category, String mobilenumber,String post,String description) {
 		ExtentReportManager.setTest(extent.createTest(testNameDetails)); // Create the test instance in Extent Reports
 		ExtentReportManager.getTest().assignAuthor(authorName); // Assign the author for the test
 		ExtentReportManager.getTest().assignCategory(category); // Assign the category for the test
@@ -30,8 +30,15 @@ public class TC_002_HomePage extends BaseClass {
 		.Select_user()
 		.Verfiy_HomePage()
 		.Click_on_Article()
-		.Click_on_writeArticle(); 
-
+		.Click_on_writeArticle()
+		.Enter_post_titel(post)
+		.Click_on_browse()
+		.Select_file()
+		.Click_on_Post_type()
+		.Add_post_description(description)
+		.Select_catogery()
+		.Click_on_perview_button()
+		.Click_on_publish_button();
 	}
 
 }
